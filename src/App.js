89c5fe0290943309c90ba518de4d29d30e0ce393
Hasanul-Alam <superdev0000@gmail.com>
2023-12-19 21:09:31 +0600

    added cart in order review page.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import './App.css';
 import Header from './components/Header/Header';
 import Shop from './components/Shop/Shop';
+import OrderReview from './components/OrderReview/OrderReview';
+import NotFound from './components/NotFound/NotFound';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Inventory from './components/Inventory/Inventory';
 
 function App() {
   return (
     <div>
       <Header></Header>
-      <Shop></Shop>
+      <BrowserRouter>
+        <Routes>
+          <Route exact path='/' element={<Shop />}/>
+          <Route path='/shop' element={<Shop />}/>
+          <Route path='/review' element={<OrderReview />}/>
+          <Route path='/inventory' element={<Inventory />}/>
+          <Route path='*' element={<NotFound />}/>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
